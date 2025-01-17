bdbb9f9f54e1f80ad95479cf4a61bc394a5e0b13
Angel Olivares <aaommago@proton.me>
2025-01-17 19:00:46 -0300

    Primer Commit

---
+++ Herencia/src/com/company/objective/Aplicacion.java
@@ -0,0 +1,13 @@
+package com.company.objective;
+
+public class Aplicacion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Vehiculo auto1 = new Vehiculo("Rojo");
+		
+		System.out.println("El color de auto1 es: "+auto1.getColor());
+
+	}
+
+}
---
+++ Herencia/src/com/company/objective/Vehiculo.java
@@ -0,0 +1,84 @@
+package com.company.objective;
+
+public class Vehiculo {
+	private int ano;
+	private String marca;
+	private String modelo;
+	private String color;
+	private int ruedas;
+	
+	
+	public Vehiculo() {}
+
+
+	public Vehiculo(String color) {
+		this.color = color;
+	}
+
+
+	public Vehiculo(int ano, String marca, String modelo, String color, int ruedas) {
+		this.ano = ano;
+		this.marca = marca;
+		this.modelo = modelo;
+		this.color = color;
+		this.ruedas = ruedas;
+	}
+
+
+	public int getAno() {
+		return ano;
+	}
+
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+
+	public String getMarca() {
+		return marca;
+	}
+
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+
+	public String getModelo() {
+		return modelo;
+	}
+
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+
+	public String getColor() {
+		return color;
+	}
+
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+
+	public int getRuedas() {
+		return ruedas;
+	}
+
+
+	public void setRuedas(int ruedas) {
+		this.ruedas = ruedas;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
